e3cce97a691a4b8d52cc248b77004b1eb21d3fa9
zhang ze <david@david.home>
2012-09-02 18:58:39 +0800

    1.update spree_multi_site's migration

--- db/migrate/20120819024114_add_site_payment_methods.spree_multi_site.rb
+++ db/migrate/20120819024114_add_site_payment_methods.spree_multi_site.rb
@@ -1,12 +1,20 @@
-# This migration comes from spree_multi_site (originally 20120818045742)
 class AddSitePaymentMethods < ActiveRecord::Migration
   # in this file add site_id after all table complete. 
   def up
     table_name = Spree::PaymentMethod.connection.table_exists?(:payment_methods) ? :payment_methods : :spree_payment_methods
     add_column table_name, :site_id, :integer
+    table_name = Spree::Configuration.connection.table_exists?(:configurations) ? :configurations : :spree_configurations
+    add_column table_name, :site_id, :integer
+    table_name = Spree::LogEntry.connection.table_exists?(:log_entries) ? :log_entries : :spree_log_entries
+    add_column table_name, :site_id, :integer
+    table_name = Spree::Preference.connection.table_exists?(:preferences) ? :preferences : :spree_preferences
+    add_column table_name, :site_id, :integer
   end
 
   def down
      remove_column Spree::PaymentMethod.table_name, :site_id
+     remove_column Spree::Configuration.table_name, :site_id
+     remove_column Spree::LogEntry.table_name, :site_id
+     remove_column Spree::Preference.table_name, :site_id
   end
 end
