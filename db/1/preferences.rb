bf4a10798e1229512d712993932f24c3488a5c22
zhang ze <david@david.home>
2012-09-16 13:59:25 +0800

    1. rake clear tmp

--- db/1/preferences.rb
+++
@@ -1,4 +0,0 @@
-#encoding: utf-8
-Spree::Config.layout='/firstshop/layouts/application'
-
-Spree::Config.allow_ssl_in_production = false
---
+++ db/firstshop/preferences.rb
@@ -0,0 +1,2 @@
+#encoding: utf-8
+Spree::Config.allow_ssl_in_production = false
