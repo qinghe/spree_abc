03eaab48057b332247140ec682ead3bbd0d7b7ed
zhang ze <david@david.home>
2012-12-16 14:45:55 +0800

    fix seed china_zone

--- db/sample/spree/china_zone.rb
+++ db/sample/spree/china_zone.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 #should not use name zones.yml, it would remove spree/core/db/spree/default/zones.yml 
 
-china =  Spree::Country.find_by_name("China")
+china =  Spree::Country.find_by_name("中国")
 zone_member = Spree::ZoneMember.new(:zoneable => china)
 china_zone = Spree::Zone.find_by_name("China")
 china_zone.zone_members << zone_member
