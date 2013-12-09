bc28cf362f20569f3ed00092777643fcd3c228d5
david <areq22@gmail.com>
2013-12-09 22:20:30 +0800

    fix typo,  'system,'->'system'

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{public/system, public/shops}
+set :linked_dirs, %w{public/system public/shops}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
@@ -29,10 +29,10 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
-      end
+      ## Here we can do anything such as:
+      #within release_path do
+      #  execute :rake, 'cache:clear'
+      #end
     end
   end
 
