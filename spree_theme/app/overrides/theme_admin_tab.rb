5815c9e3a4d174d81fe62c296b61ded08996defc
david <areq22@gmail.com>
2014-01-03 22:11:51 +0800

    add locales for spree_themes

--- spree_theme/app/overrides/theme_admin_tab.rb
+++ spree_theme/app/overrides/theme_admin_tab.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "theme_admin_tab",
+                     :class => "tab-with-icon",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:template_themes) %>",
+                     :text => "<%= tab(:template_themes,:icon=>'icon-theme') %>",
                      :disabled => false)
