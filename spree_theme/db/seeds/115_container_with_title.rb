3b2ef8b0beb42dbabfadae086f56d20427593551
david <areq22@gmail.com>
2014-02-21 21:05:11 +0800

    fix view user_password/_new

--- spree_theme/db/seeds/115_container_with_title.rb
+++ spree_theme/db/seeds/115_container_with_title.rb
@@ -4,6 +4,6 @@ section_piece_hash= Spree::SectionPiece.all.inject({}){|h,sp| h[sp.slug] = sp; h
 
 #container-title
 Spree::Section.where(:title=>'container with title').each(&:destroy)
-order_address = Spree::Section.create_section(section_piece_hash['container'].id, {:title=>"container with title"},
+container = Spree::Section.create_section(section_piece_hash['container'].id, {:title=>"container with title"},
   {'block'=>{'disabled_ha_ids'=>'111'}, 'inner'=>{'15hidden'=>bool_true}})
-order_address.add_section_piece(section_piece_hash['container-title'].id)
+container.add_section_piece(section_piece_hash['container-title'].id)
