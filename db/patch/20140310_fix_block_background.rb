99fc602e44a1a02cb84f541a78e8f68b8b8ee27b
david <areq22@gmail.com>
2014-03-18 22:53:25 +0800

    fix patch

--- db/patch/20140310_fix_block_background.rb
+++ db/patch/20140310_fix_block_background.rb
@@ -1,3 +1,4 @@
 spp = Spree::SectionPieceParam.where(:section_piece_id=>2, :editor_id=>3, :class_name=>"block").first
-
-spp.update_attribute(:class_name, "inner")
+if spp
+  spp.update_attribute(:class_name, "inner")
+end
