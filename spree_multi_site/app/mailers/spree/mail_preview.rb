1cd90711000ed3cc040555b527f5b564e165e637
david <areq22@gmail.com>
2014-03-26 22:35:29 +0800

    fix mail preview

--- spree_multi_site/app/mailers/spree/mail_preview.rb
+++ spree_multi_site/app/mailers/spree/mail_preview.rb
@@ -1,9 +1,11 @@
-class MailPreview < MailView
-  def load_sample()
-    site = Spree::Site.create!(:name=>"ABC",:domain=>"www.example.com")
-    user = site.users.create!(:email=>'abc@example.com',:password=>"spree123",:password_confirmation=>"spree123")
-    mail = Spree::UserMailer.load_sample(user)
-    site.destroy
-    mail
+module Spree
+  class MailPreview < MailView
+    def load_sample()
+      site = Spree::Site.create!(:name=>"ABC",:domain=>"www.example.com")
+      user = site.users.create!(:email=>'abc@example.com',:password=>"spree123",:password_confirmation=>"spree123")
+      mail = Spree::UserMailer.load_sample(user)
+      site.destroy
+      mail
+    end
   end
 end
