ae20b13600ec80cc74a965913cc1d0f3fda25c2f
david <areq22@gmail.com>
2014-03-26 20:38:59 +0800

    add template_text.default_scope

--- spree_theme/app/models/spree/template_text.rb
+++ spree_theme/app/models/spree/template_text.rb
@@ -4,6 +4,6 @@ module Spree
     attr_accessible :name, :body
     #for resource_class.resourceful
     scope :resourceful, ->(theme){ where("1=1") }
-    
+    default_scope ->{ where(:site_id=>Site.current.id)}
   end
 end
