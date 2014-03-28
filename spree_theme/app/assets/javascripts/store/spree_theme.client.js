0df86847a525c3e682cbcb8580b9d2af17dd838f
david <areq22@gmail.com>
2014-03-28 23:46:53 +0800

    fix assets precompile

--- spree_theme/app/assets/javascripts/store/spree_theme.client.js
+++ spree_theme/app/assets/javascripts/store/spree_theme.client.js
@@ -1,10 +1,3 @@
-//= require jquery
-//= require jquery.ui.all
-//= require jquery_ujs
-//= require jquery.form
-//= require jquery.layout
-//= require jquery.ajax
-//= require store/spree_frontend
 //= require jquery.validate/localization/messages_zh-CN.js
 function center_template_section( selector )
 {
