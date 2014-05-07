50ecdd2e3849aa2fde832895267c2c625a6a5066
david <areq22@gmail.com>
2014-05-07 22:49:14 +0800

    add template_text into shopfirst seed,

--- db/shopfirst/seed.rb
+++ db/shopfirst/seed.rb
@@ -1,4 +1,4 @@
-files = [ "sites", "users" ]
+files = [ "sites", "users","template_text" ]
 for file in files
   path = File.join(File.dirname(__FILE__), "#{file}.rb")
 Rails.logger.debug "start load #{file}"     
