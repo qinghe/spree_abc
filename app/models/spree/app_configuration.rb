e24c08595e297aabdb9bc99b531fa858383d40c1
david <areq22@gmail.com>
2014-06-19 00:09:25 +0800

    1. add more doc 2. add logo favicon

--- app/models/spree/app_configuration.rb
+++ app/models/spree/app_configuration.rb
@@ -22,7 +22,8 @@ module Spree
 
     # Alphabetized to more easily lookup particular preferences
     preference :address_requires_state, :boolean, default: true # should state/state_name be required
-    preference :admin_interface_logo, :string, default: 'logo/spree_50.png'
+#    preference :admin_interface_logo, :string, default: 'logo/spree_50.png'
+    preference :admin_interface_logo, :string, default: 'logo/logo2c.gif'
     preference :admin_products_per_page, :integer, default: 10
     preference :allow_backorder_shipping, :boolean, default: false # should only be true if you don't need to track inventory
     preference :allow_checkout_on_gateway_error, :boolean, default: false
@@ -61,7 +62,8 @@ module Spree
     preference :last_check_for_spree_alerts, :string, default: nil
 #    preference :layout, :string, default: 'spree/layouts/spree_application'
     preference :layout, :string, default: '/layouts/abc_application.html.erb'
-    preference :logo, :string, default: 'logo/spree_50.png'
+#    preference :logo, :string, default: 'logo/spree_50.png'
+    preference :logo, :string, default: 'logo/dalianshops.gif'
     preference :max_level_in_taxons_menu, :integer, default: 1 # maximum nesting level in taxons menu
     preference :orders_per_page, :integer, default: 15
     preference :prices_inc_tax, :boolean, default: false
