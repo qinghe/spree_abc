b1853ff8a8d316170024a3e43a01d544d2e319fe
david <areq22@gmail.com>
2014-07-22 08:26:57 +0800

    same as last

--- spree_theme/app/controllers/spree/admin/page_layout_controller.rb
+++ spree_theme/app/controllers/spree/admin/page_layout_controller.rb
@@ -7,18 +7,36 @@ module Spree
           @theme = TemplateTheme.find( params[:template_theme_id])          
           assigned_resource_ids = params[:assigned_resource_ids].try(:[], @page_layout.id.to_s)
           #assign taxon
-          section_piece = @page_layout.section_pieces.with_resources.first
+          section_piece_with_resources = @page_layout.section_pieces.with_resources.first
           if assigned_resource_ids.kind_of? Array
-            section_piece.wrapped_resources.each_with_index{|wrapped_resource,index|
-              resource_id = assigned_resource_ids[index].to_i
-              resource_class = wrapped_resource.resource_class               
-              if resource_id>0
-                resource = resource_class.find resource_id
-                @theme.assign_resource(resource, @page_layout, index)
-              else
-                @theme.unassign_resource(resource_class, @page_layout, index)
-              end              
-            }
+            if section_piece_with_resources.present?
+              section_piece.wrapped_resources.each_with_index{|wrapped_resource,index|
+                resource_id = assigned_resource_ids[index].to_i
+                resource_class = wrapped_resource.resource_class               
+                if resource_id>0
+                  resource = resource_class.find resource_id
+                  @theme.assign_resource(resource, @page_layout, index)
+                else
+                  @theme.unassign_resource(resource_class, @page_layout, index)
+                end              
+              }
+            else# assigned taxon root or specific taxon
+              assigned_resource_ids.each_with_index{|resource_id, index|
+                resource_id = resource_id.to_i
+                if resource_id>0
+                  resource = SpreeTheme.taxon_class.find resource_id
+                  if resource.root?
+                    @theme.assign_resource(resource, @page_layout, index)                    
+                  else
+                    resource = Spree::SpecificTaxon.find resource_id
+                    @theme.assign_resource(resource, @page_layout, index)    
+                  end
+                  
+                else
+                  @theme.unassign_resource(resource_class, @page_layout, index)
+                end           
+              }
+            end
           end
           respond_to do |format|
             format.js  { render :text => 'Ok' }
