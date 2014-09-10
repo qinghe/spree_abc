42706d6cf2a739369c06cc87855e690f325f3b7b
david <david@dalian-david.onsute.com>
2014-09-10 02:49:18 -0700

    fix add to cart

--- spree_flexi_variants/app/models/spree/order_decorator.rb
+++ spree_flexi_variants/app/models/spree/order_decorator.rb
@@ -1,37 +1,5 @@
 module Spree
   Order.class_eval do
-    def add_variant(variant, quantity = 1, ad_hoc_option_value_ids=[], product_customizations=[])
-      current_item = contains?(variant, ad_hoc_option_value_ids, product_customizations)
-      if current_item
-        current_item.quantity += quantity
-        current_item.save
-      else
-        current_item = LineItem.new(:quantity => quantity)
-        current_item.variant = variant
-
-        # add the product_customizations, if any
-        # TODO: is this an unnecessary step?
-        product_customizations.map(&:save) # it is now safe to save the customizations we created in the OrdersController.populate
-
-        current_item.product_customizations = product_customizations
-
-        # find, and add the configurations, if any.  these have not been fetched from the db yet.              line_items.first.variant_id
-        # we postponed it (performance reasons) until we actaully knew we needed them
-        povs=[]
-        ad_hoc_option_value_ids.each do |cid|
-          povs << AdHocOptionValue.find(cid)
-        end
-        current_item.ad_hoc_option_values = povs
-
-        current_item.price   = variant.price + povs.map(&:price_modifier).compact.sum + product_customizations.map {|pc| pc.price(variant)}.sum
-        self.line_items << current_item
-      end
-      current_item
-    end
-
-    def contains?(variant, ad_hoc_option_value_ids, product_customizations)
-      find_line_item_by_variant(variant, ad_hoc_option_value_ids, product_customizations).present?
-    end
 
     def find_line_item_by_variant(variant, ad_hoc_option_value_ids=[], product_customizations=[])
       line_items.detect do |li|
