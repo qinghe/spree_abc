013bb62e6bf54e790e289bea28835c7197d36f33
david <areq22@gmail.com>
2014-09-13 22:35:15 +0800

    add feature customization option. upgrate spree

--- spree_theme/app/models/page_tag/products.rb
+++ spree_theme/app/models/page_tag/products.rb
@@ -5,7 +5,7 @@ module PageTag
   class Products < ModelCollection
     
     class WrappedProduct < WrappedModel
-      self.accessable_attributes=[:id,:name,:description,:images,:variant_images,:has_variants?,:price_in, :price, :master, :currency, :variants_and_option_values, :grouped_option_values,:variants_for_option_value, :total_on_hand,:variant_options_hash ] 
+      self.accessable_attributes=[:id,:name,:description,:images,:variant_images,:has_variants?,:price_in, :price, :master, :currency, :variants_and_option_values, :grouped_option_values,:variants_for_option_value, :total_on_hand,:variant_options_hash,:product_customization_types ] 
       delegate *self.accessable_attributes, :to => :model
       
       #:model_name use by small_image
