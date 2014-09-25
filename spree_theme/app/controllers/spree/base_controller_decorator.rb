1fb882acf4b2b5de219d2c2a3001888e2f2bc1a6
david <david@dalian-david.onsute.com>
2014-09-24 20:55:49 -0700

    compile assets admin_login

--- spree_theme/app/controllers/spree/base_controller_decorator.rb
+++ spree_theme/app/controllers/spree/base_controller_decorator.rb
@@ -1,21 +1,31 @@
 #encoding: utf-8
 module Spree
-  module Admin
     BaseController.class_eval do
-      rescue_from CanCan::AccessDenied do |exception|
-        return spree_theme_admin_unauthorized
-      end  
+      #rescue_from CanCan::AccessDenied do |exception|
+      #  return spree_theme_admin_unauthorized
+      #end  
       
-      def spree_theme_admin_unauthorized
+      #def spree_theme_admin_unauthorized
+      #  Rails.logger.debug "yes, it is calling spree_theme_admin_unauthorized"
+      #    if try_spree_current_user
+      #      flash[:error] = Spree.t(:authorization_failure)
+      #      redirect_to '/unauthorized'
+      #    else
+      #      store_location
+      #      url = new_admin_session_path
+      #      redirect_to url
+      #    end
+      #end
+      #override spree_core/controller_helper/auth#unauthorized
+        def unauthorized
+          url = new_admin_session_path
           if try_spree_current_user
             flash[:error] = Spree.t(:authorization_failure)
-            redirect_to '/unauthorized'
+            redirect_to new_admin_session_path
           else
             store_location
-            url = new_admin_session_path
-            redirect_to url
+            redirect_to new_admin_session_path
           end
-      end  
+        end  
     end
-  end
 end
