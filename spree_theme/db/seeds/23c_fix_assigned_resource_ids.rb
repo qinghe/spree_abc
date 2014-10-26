515c4a011a95f038ac6ba9c9db78f42a33d0dac7
david <areq22@gmail.com>
2014-10-26 16:59:13 +0800

    fix template.assigned_resource_ids

---
+++ spree_theme/db/seeds/23c_fix_assigned_resource_ids.rb
@@ -0,0 +1,10 @@
+Spree::Site.all.each{|site|
+  site.template_themes.each{|template_theme|
+    template_theme.assigned_resource_ids.each_pair{|key,val|      
+      # fix :undefined method `classify' for :"spree/taxon":Symbol
+      #spree_theme/app/models/spree/template_resource.rb:44:in `source_class'
+      val.stringify_keys!
+    }
+    template_theme.save!
+  }
+}
