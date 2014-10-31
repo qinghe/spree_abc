b957c9afffd4a85563da8d52c3740bb40b447047
david <david@dalian-david.onsute.com>
2014-10-31 00:18:46 -0700

    add more tests

---
+++ spree_multi_site/spec/models/taxon_spec.rb
@@ -0,0 +1,12 @@
+#encoding: utf-8
+require 'spec_helper'
+describe Spree::Taxon do
+  before(:each) do
+    Spree::Site.current = Spree::Site.create!(:name=>'ABCD',:domain=>'www.abc.net')
+    @taxon = Spree::Taxon.new(:name=>'ABCD')
+  end
+  it "should create taxon with valid site!" do
+    new_taxon = Spree::Taxon.create!({ taxonomy_id: 0, name: 'name' }, without_protection: true) 
+    new_taxon.site.should eq Spree::Site.current
+  end  
+end
