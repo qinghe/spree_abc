5f0cd717dac6cb6b9bcee8ee7f2836dbec223057
david <david@dalian-david.onsute.com>
2014-12-23 03:09:34 -0800

    add help SeedHelper

--- spree_theme/lib/spree_theme/seed_helper.rb
+++ spree_theme/lib/spree_theme/seed_helper.rb
@@ -1,10 +1,22 @@
 module SpreeTheme
-  module SectionPieceParamHelper
+  module SeedHelper
+    
     def create_section_piece_param( section_piece, section_piece_param_attrs)
       section_piece.section_piece_params.create! do|spp|
         spp.param_conditions={}
-        spp.assign_attributes( section_piece_param_attrs,  :without_protection => true)
+        spp.assign_attributes( section_piece_param_attrs )
       end
     end  
+    
+    
+    def find_section_piece( slug )
+      section_piece_hash= Spree::SectionPiece.all.inject({}){|h,sp| h[sp.slug] = sp; h}
+      section_piece_hash[slug]
+    end
+    
+    def find_html_attribute( slug )
+      Spree::HtmlAttribute.friendly.find 'font-weight'
+    end
+    
   end
 end
