bed4bda042abd1196125b94d3d70697c857adebd
david <areq22@gmail.com>
2015-01-12 22:39:45 +0800

    remove unused code

--- config/environments/local_staging.rb
+++ config/environments/local_staging.rb
@@ -60,9 +60,6 @@ SpreeAbc::Application.configure do
   
   config.after_initialize do
     Spree::Site.system_top_domain = 'david.com'  
-    # work around https://github.com/spree-contrib/spree_editor/commit/6813ec0ace000f6ef6b28219ff6e5a101056531b
-    # only asset with full url could be substitude with digest
-    #Rails.application.config.action_controller.asset_host = Spree::Site.system_top_domain
   end
 
   
