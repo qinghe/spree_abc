b385dd1932be029dfaf0aff1c93178050dab4aae
david <areq22@gmail.com>
2015-01-14 23:21:53 +0800

    fix api product list

--- spree_theme/app/controllers/spree/decorators/api_products_controller_decorator.rb
+++ spree_theme/app/controllers/spree/decorators/api_products_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::Api::ProductsController.class_eval do
     
     # get products assigned to default home page
     home_page = Spree::Site.current.try(:template_theme).try(:mobile).try(:home_page)
-    @products.in_taxon(home_page) if home_page
+    @products = @products.in_taxon(home_page) unless home_page.blank?
     
     @products = @products.ransack(params[:q]).result if params[:q]
     @products = @products.distinct.page(params[:page]).per(params[:per_page])
