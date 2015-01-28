47a9115f73a1dd9210249b4c32384c206da1279a
qinghe <areq22@gmail.com>
2015-01-27 20:08:45 -0800

    add more ckeditor config

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,91 @@
 CKEDITOR.editorConfig = function( config ) {
+    // copied from original config.js ckeditor 4.0.11  
+    
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    /* Filebrowser routes */
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+    config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+
+    // The location of a script that handles file uploads in the Flash dialog.
+    config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+
+    // The location of a script that handles file uploads in the Image dialog.
+    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+
+    // The location of a script that handles file uploads.
+    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+    // Rails CSRF token
+    config.filebrowserParams = function(){
+      var csrf_token, csrf_param, meta,
+          metas = document.getElementsByTagName('meta'),
+          params = new Object();
+
+      for ( var i = 0 ; i < metas.length ; i++ ){
+        meta = metas[i];
+
+        switch(meta.name) {
+          case "csrf-token":
+            csrf_token = meta.content;
+            break;
+          case "csrf-param":
+            csrf_param = meta.content;
+            break;
+          default:
+            continue;
+        }
+      }
+
+      if (csrf_param !== undefined && csrf_token !== undefined) {
+        params[csrf_param] = csrf_token;
+      }
+
+      return params;
+    };
+
+    config.addQueryString = function( url, params ){
+      var queryString = [];
+
+      if ( !params ) {
+        return url;
+      } else {
+        for ( var i in params )
+          queryString.push( i + "=" + encodeURIComponent( params[ i ] ) );
+      }
+
+      return url + ( ( url.indexOf( "?" ) != -1 ) ? "&" : "?" ) + queryString.join( "&" );
+    };
+
+    // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
+    CKEDITOR.on( 'dialogDefinition', function( ev ){
+      // Take the dialog name and its definition from the event data.
+      var dialogName = ev.data.name;
+      var dialogDefinition = ev.data.definition;
+      var content, upload;
+
+      if (CKEDITOR.tools.indexOf(['link', 'image', 'attachment', 'flash'], dialogName) > -1) {
+        content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
+        upload = (content == null ? null : content.get('upload'));
+
+        if (upload && upload.filebrowser && upload.filebrowser['params'] === undefined) {
+          upload.filebrowser['params'] = config.filebrowserParams();
+          upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
+        }
+      }
+    });
+        
     config.language = 'zh-cn';   
     // Default setting.
     //config.toolbarGroups = [
@@ -19,7 +106,6 @@ CKEDITOR.editorConfig = function( config ) {
     //    //{ name: 'about' }
     //];
 
-    config.toolbar = 'Full';   
     config.toolbar_Full = [ 
       { name: 'document', items : [ 'Source','-','NewPage','DocProps','Preview','Templates' ] },
       { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] }, 
@@ -38,6 +124,6 @@ CKEDITOR.editorConfig = function( config ) {
       //{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] } 
 
     ];
-
+    config.toolbar = 'Full';   
 
 };
