d9d006e17a630cf78710579ca26e10d23579ea6a
qinghe <areq22@gmail.com>
2015-03-01 23:15:04 -0800

    remove unused code

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-SpreeAbc::Application.config.session_store :active_record_store, key: '_dalianshops_2.4', domain: :all
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# SpreeAbc::Application.config.session_store :active_record_store
