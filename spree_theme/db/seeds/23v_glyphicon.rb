bee5ff56306891cf34d730c94a0ac9af4dfe4ef1
david <areq22@gmail.com>
2015-03-15 23:23:38 +0800

    add section glyphicon

---
+++ spree_theme/db/seeds/23v_glyphicon.rb
@@ -0,0 +1,4 @@
+
+post_summary = Spree::Section.create_section(section_piece_hash['container'], {:title=>"bootstrap glyphicon"})
+   
+post_summary.add_section_piece(section_piece_hash['bootstrap-glyphicon'])
