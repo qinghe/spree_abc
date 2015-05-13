4085fb69363fb6d4d070b4b56aaef096a6558cfc
qinghe <areq22@gmail.com>
2015-05-12 20:08:00 -0700

    improve admin login style

--- config/environments/local_staging.rb
+++ config/environments/local_staging.rb
@@ -44,7 +44,7 @@ SpreeAbc::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( tinymce-jquery.js )
-  config.assets.precompile += %w( admin_login.css html5shiv.js )
+  config.assets.precompile += %w( html5shiv.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
