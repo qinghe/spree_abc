758887bd9b456c3fd01b0e9d56046d8a95f00e2e
qinghe <areq22@gmail.com>
2015-05-22 05:28:32 -0700

    enable flexi_variants, fix add_to_cart

--- spree_flexi_variants/app/models/spree/decorators/order_decorator.rb
+++ spree_flexi_variants/app/models/spree/decorators/order_decorator.rb
@@ -1,7 +1,39 @@
 module Spree
   Order.class_eval do
+    def add_variant(variant, quantity = 1, ad_hoc_option_value_ids=[], product_customizations=[])
+      current_item = find_line_item_by_variant(variant, ad_hoc_option_value_ids, product_customizations)
+      if current_item
+        current_item.quantity += quantity
+        current_item.save
+      else
+        current_item = LineItem.new(:quantity => quantity)
+        current_item.variant = variant
 
-    def find_line_item_by_variant(variant, ad_hoc_option_value_ids=[], product_customizations=[])
+        # add the product_customizations, if any
+        # TODO: is this an unnecessary step?
+        product_customizations.map(&:save) # it is now safe to save the customizations we created in the OrdersController.populate
+
+        current_item.product_customizations = product_customizations
+
+        # find, and add the configurations, if any.  these have not been fetched from the db yet.              line_items.first.variant_id
+        # we postponed it (performance reasons) until we actaully knew we needed them
+        povs=[]
+        ad_hoc_option_value_ids.each do |cid|
+          povs << AdHocOptionValue.find(cid)
+        end
+        current_item.ad_hoc_option_values = povs
+
+        current_item.price   = variant.price + povs.map(&:price_modifier).compact.sum + product_customizations.map {|pc| pc.price(variant)}.sum
+        self.line_items << current_item
+      end
+      current_item
+    end
+
+    def contains?(variant, ad_hoc_option_value_ids, product_customizations)
+      find_line_item_by_variant(variant, ad_hoc_option_value_ids, product_customizations).present?
+    end
+
+    def find_line_item_by_variant(variant, ad_hoc_option_value_ids, product_customizations)
       line_items.detect do |li|
         li.variant_id == variant.id &&
           matching_configurations(li.ad_hoc_option_values,ad_hoc_option_value_ids) &&
@@ -9,21 +41,11 @@ module Spree
       end
     end
 
-    # copy original method merge! here, only change the way get current_line_item
     def merge!(order, user = nil)
       order.line_items.each do |line_item|
-        next unless line_item.currency == currency
-        # change the way get current_line_item
-        current_line_item = find_line_item_by_variant( line_item.variant, line_item.ad_hoc_option_value_ids, line_item.product_customizations )
-        if current_line_item
-          current_line_item.quantity += line_item.quantity
-          current_line_item.save
-        else
-          line_item.order_id = self.id
-          line_item.save
-        end
+        self.add_variant(line_item.variant, line_item.quantity, line_item.ad_hoc_option_value_ids, line_item.product_customizations)
       end
-      
+
       self.associate_user!(user) if !self.user && !user.blank?
 
       # So that the destroy doesn't take out line items which may have been re-assigned
