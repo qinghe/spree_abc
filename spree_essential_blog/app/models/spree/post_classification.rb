391acadce5e88080b37cd6b104289ee1b17de3e8
qinghe <areq22@gmail.com>
2015-05-23 19:07:52 +0800

    fix missing post position

--- spree_essential_blog/app/models/spree/post_classification.rb
+++ spree_essential_blog/app/models/spree/post_classification.rb
@@ -4,7 +4,7 @@ module Spree
     self.table_name = 'spree_posts_taxons'
     acts_as_list scope: :taxon
     belongs_to :post, class_name: "Spree::Post", inverse_of: :post_classifications, touch: true
-    belongs_to :taxon, class_name: "Spree::Taxon", inverse_of: :post_classifications, touch: true
+    belongs_to :taxon, class_name: "Spree::Taxon", touch: true
 
     # For #3494
     validates_uniqueness_of :taxon_id, :scope => :post_id, :message => :already_linked
