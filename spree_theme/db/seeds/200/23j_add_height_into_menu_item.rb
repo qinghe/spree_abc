c88c44a130722e0ad80b235c5f68d78d0a0d45cb
qinghe <areq22@gmail.com>
2015-07-08 02:36:32 -0700

    fix seed cause by miss change

--- spree_theme/db/seeds/200/23j_add_height_into_menu_item.rb
+++ spree_theme/db/seeds/200/23j_add_height_into_menu_item.rb
@@ -1,10 +1,8 @@
 # add html_attribute margin into a
-spps = Spree::SectionPieceParam.where(:editor_id=>4,:class_name=>'block').all
+spps = Spree::SectionPieceParam.where(:editor_id=>2,:class_name=>'a').all
 
 if spps.size == 1
-  letter_space = Spree::HtmlAttribute.find 52
-  line_height = Spree::HtmlAttribute.find 16  #height
-  spps.first.insert_html_attribute line_height, letter_space
-else
-  raise 'more than one section piece param named block and editor is 4'
+  margin = Spree::HtmlAttribute.find 31  #margin
+  height = Spree::HtmlAttribute.find 15  #height
+  spps.first.insert_html_attribute height, margin
 end
--- spree_theme/db/seeds/250/257_line_height.rb
+++ spree_theme/db/seeds/250/257_line_height.rb
@@ -1,8 +1,10 @@
 # add html_attribute margin into a
-spps = Spree::SectionPieceParam.where(:editor_id=>2,:class_name=>'a').all
+spps = Spree::SectionPieceParam.where(:editor_id=>4,:class_name=>'block').all
 
 if spps.size == 1
-  margin = Spree::HtmlAttribute.find 31  #margin
-  height = Spree::HtmlAttribute.find 15  #height
-  spps.first.insert_html_attribute height, margin
+  letter_space = Spree::HtmlAttribute.find 52
+  line_height = Spree::HtmlAttribute.find 16  #height
+  spps.first.insert_html_attribute line_height, letter_space
+else
+  raise 'more than one section piece param named block and editor is 4'
 end
