787f51e8e1c89cbf4181ece86506421eaaebaed2
qinghe <areq22@gmail.com>
2015-07-15 03:20:39 -0700

    fix seed sider

--- spree_theme/db/seeds/250/256_add_sider.rb
+++ spree_theme/db/seeds/250/256_add_sider.rb
@@ -1,13 +1,13 @@
 #fixed_container
-Spree::Section.where(:title=>'fixed container').each(&:destroy)
-fixed_container = Spree::Section.create_section( section_piece_hash['container'], {:title=>"fixed container"},
+#Spree::Section.where(:title=>'fixed container stupid').each(&:destroy)
+fixed_container = Spree::Section.create_section( section_piece_hash['container'], {:title=>"fixed container stupid"},
 { 'content_layout'=>{'86'=>bool_true,'86unset'=>bool_false}, 'block'=>{'15'=>"height:100px",'15unset'=>bool_false,'101'=>"float:left",'101unset'=>bool_false}, 'inner'=>{'15hidden'=>bool_true}}
 )
 fixed_container.add_section_piece(section_piece_hash['container-fixed'])
 
 
 #hover_effect_container
-Spree::Section.where(:title=>'click effect sider container').each(&:destroy)
+#Spree::Section.where(:title=>'click effect sider container').each(&:destroy)
 container = Spree::Section.create_section( section_piece_hash['container'], {:title=>"click effect sider container", :usage =>'container', :content_param=>  Spree::Section::MouseEffect.sider+1 },
   { 'content_layout'=>{'86'=>bool_true,'86unset'=>bool_false}, 'block'=>{'101'=>"float:left",'101unset'=>bool_false}, 'inner'=>{'15hidden'=>bool_true,'21hidden'=>bool_true}}
 )
--- spree_theme/db/seeds/250/259_mobile_root_params.rb
+++ spree_theme/db/seeds/250/259_mobile_root_params.rb
@@ -1,7 +1,7 @@
 section_piece = find_section_piece('root-for-mobile')
 
-inner =  { "editor_id"=>2,  "class_name"=>"inner", "pclass"=>"css", "param_category_id"=>6,  "html_attribute_ids"=>"32,7,8,6"}
+inner =  { "editor_id"=>2,  "class_name"=>"inner", "pclass"=>"css", "param_category_id"=>2,  "html_attribute_ids"=>"32,7,8,6"}
 create_section_piece_param( section_piece, inner)
 
-inner =  { "editor_id"=>3, "class_name"=>"inner", "pclass"=>"css", "param_category_id"=>6,  "html_attribute_ids"=>"2,3,4,5"}
+inner =  { "editor_id"=>3, "class_name"=>"inner", "pclass"=>"css", "param_category_id"=>2,  "html_attribute_ids"=>"2,3,4,5"}
 create_section_piece_param( section_piece, inner)
