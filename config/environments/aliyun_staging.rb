a866c2545df191f4663fcd54d90faea7aad4143c
zhang ze <david@www.david.com>
2015-08-12 00:57:18 +0800

    use aliasscdn.firecart.cn

--- config/environments/aliyun_staging.rb
+++ config/environments/aliyun_staging.rb
@@ -40,7 +40,7 @@ SpreeAbc::Application.configure do
   config.cache_store = :memory_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://aliass.firecart.cn"
+  config.action_controller.asset_host = "http://aliasscdn.firecart.cn"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( html5shiv.js )
