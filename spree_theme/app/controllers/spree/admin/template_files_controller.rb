18326802b4f5adb793478751842bfcc1a655375a
zhang ze <david@www.david.com>
2015-08-22 13:00:18 +0800

    add feature template_file pagination

--- spree_theme/app/controllers/spree/admin/template_files_controller.rb
+++ spree_theme/app/controllers/spree/admin/template_files_controller.rb
@@ -4,7 +4,14 @@ module Spree
 
 
       def collection
-        model_class.where( ["theme_id in (?)", Spree::Site.current.template_theme_ids] ).includes(:template_theme).order('theme_id')
+        return @collection if @collection.present?
+        # params[:q] can be blank upon pagination
+        params[:q] = {} if params[:q].blank?
+
+        @collection = model_class.where( ["theme_id in (?)", Spree::Site.current.template_theme_ids] ).includes(:template_theme)
+        @search = @collection.ransack(params[:q])
+
+        @search.result.page(params[:page]).per(Spree::Config[:orders_per_page]).order('theme_id')
       end
     end
   end
