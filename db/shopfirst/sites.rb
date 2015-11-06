84f3a273bff19875c5aadf6829d2d06dd1118832
qinghe <areq22@aliyun.com>
2015-11-06 02:02:44 -0800

    set is_public to true for store 1,2,3

--- db/shopfirst/sites.rb
+++ db/shopfirst/sites.rb
@@ -1,20 +1,22 @@
-attributes = {  
+attributes = {
     :name =>  "Dalian shops",
     :domain => "www.dalianshops.com",
     :short_name => "first",
     :email => "first@dalianshops.com",
     :password =>'spree123'
   }
-site = Spree::Site.create!(attributes)    
+site = Spree::Site.create!(attributes)
 Spree::Site.current = site
+
+Spree::Site.current.stores.first.update_attribute(:is_public,true)
 
 # see last line where we create an admin if there is none, asking for email and password
 =begin
 def create_site
-  
+
   name = 'Abc'
-  domain = Spree::Site.abc_domain    
-  
+  domain = Spree::Site.abc_domain
+
   attributes = {
     :name => name,
     :domain => domain
@@ -25,14 +27,14 @@ def create_site
   if Spree::Site.find_by_domain(domain)
     say "\nWARNING: There is already a site with the domain: #{domain}, so no site changes were made.  If you wish to create an additional site, please run rake db:site:create again with a different domain.\n\n"
   else
-    admin = Spree::Site.create!(attributes)    
+    admin = Spree::Site.create!(attributes)
   end
 end
 
 if Spree::Site.admin.blank?
   create_site
   #creating user require it.
-  Spree::Site.current =Spree::Site.admin 
+  Spree::Site.current =Spree::Site.admin
 else
   puts "Default site #{domain} has already been previously created."
 end
