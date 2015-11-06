70917c914886ef41f975f35fc0dd279685bc7be7
qinghe <areq22@aliyun.com>
2015-11-06 01:51:32 -0800

    design shop should be public by default

--- db/shopdesign/sites.rb
+++ db/shopdesign/sites.rb
@@ -1,11 +1,12 @@
-attributes = {  
+attributes = {
     :name =>  "Dalianshops design",
     :domain => "design.dalianshops.com",
     :short_name => "design",
     :email => 'design@dalianshops.com',
     :password => 'spree123'
   }
-site = Spree::Site.create!(attributes) 
-   
+site = Spree::Site.create!(attributes)
+
 Spree::Site.current = site
 
+Spree::Site.current.stores.first.update_attribute(:is_public,true)
