ccc6bceda35c32ca676e5abe5641f457080a5c27
qinghe <areq22@aliyun.com>
2015-11-17 23:47:24 -0800

    add routes for pingpp webhook

--- spree_pingpp/config/routes.rb
+++ spree_pingpp/config/routes.rb
@@ -2,6 +2,8 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   patch '/checkout/handle_pingpp', :to => 'checkout#handle_pingpp', as: :handle_pingpp, format: :json
 
+  # called by pingpp webhook
+  post  '/pingpp/charge_notify', :to=> 'pingpp_status#charge_notify'
   # alipay get,  upacp_pc post
   match '/pingpp/charge_done', :to=> 'pingpp_status#charge_done', as: :pingpp_charge_done,  via: [:get, :post]
 
