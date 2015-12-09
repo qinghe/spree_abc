1d8ec9f2aeadec5adbae007a3dabe1648573611c
david <areq22@aliyun.com>
2015-12-09 21:02:54 +0800

    use cache store  file_store

--- config/environments/aliyun_staging.rb
+++ config/environments/aliyun_staging.rb
@@ -37,7 +37,7 @@ SpreeAbc::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :memory_store
+  # config.cache_store = :memory_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = "http://aliasscdn.getstore.cn"
