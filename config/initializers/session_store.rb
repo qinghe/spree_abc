5e34e5942e7f10b8d843d0073e4f92857bdbd01a
qinghe <areq22@aliyun.com>
2015-12-14 23:38:25 -0800

    rename cookie to getstore_3_0, disable domain:all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-SpreeAbc::Application.config.session_store :active_record_store, key: '_dalianshops_2.4', domain: :all
+# we should disable domain: :all,
+# fix case: after loggin design.getstore.cn/admin,  open new tab getstore.cn,
+# then the design.getstore.cn/admin become unauthenticate
+SpreeAbc::Application.config.session_store :active_record_store, key: '_GetStore_3_0' #, domain: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
