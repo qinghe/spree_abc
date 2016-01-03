afdc419e977d40984ac4322ce5796ddde686914d
david <areq22@aliyun.com>
2016-01-03 13:38:19 +0800

    fix error caused by serializable fields with default '' value

--- db/migrate/20151208091444_null_serializable_fields.spree_theme.rb
+++ db/migrate/20151208091444_null_serializable_fields.spree_theme.rb
@@ -1,6 +1,6 @@
 class NullSerializableFields < ActiveRecord::Migration
   def change
     # default value should be nil or value translatable to hash, default '' would cause ActiveRecord::SerializationTypeMismatch on 4.2.2
-    change_column :spree_template_themes, :assigned_resource_ids , :string, :limit => 2048, :null => true
+    change_column :spree_template_themes, :assigned_resource_ids , :string, :limit => 2048, :null => true, :default=> nil
   end
 end
