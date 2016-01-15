40e3347c32fc7974e488a40e207fe8124f399c5a
david <areq22@aliyun.com>
2016-01-15 20:07:41 +0800

    add feature authorize site when edit page_layout

--- app/controllers/decorators/authorize_site_decorator.rb
+++ app/controllers/decorators/authorize_site_decorator.rb
@@ -1,11 +1,24 @@
+Spree::PageLayoutsController.class_eval do
+  #store could preview native template theme
+  before_filter :authorize_site
+
+  def authorize_site
+    # Site.current.god? would not work, god site loaded for unexist domain
+    unless Spree::Store.current.designable?
+      redirect_to 'http://'+Spree::Store.god.subdomain, status: :moved_permanently
+      #raise CanCan::AccessDenied.new("Not authorized!", :access, Site)
+    end
+  end
+end
+
 Spree::TemplateThemesController.class_eval do
   #store could preview native template theme
   #before_filter :authorize_site
 
   #def authorize_site
   #  # Site.current.god? would not work, god site loaded for unexist domain
-  #  unless Store.current.designable?
-  #    redirect_to 'http://'+Store.god.subdomain, status: :moved_permanently
+  #  unless Spree::Store.current.designable?
+  #    redirect_to 'http://'+Spree::Store.god.subdomain, status: :moved_permanently
   #    #raise CanCan::AccessDenied.new("Not authorized!", :access, Site)
   #  end
   #end
--- spec/controllers/spree/authorize_site_spec.rb
+++ spec/controllers/spree/authorize_site_spec.rb
@@ -4,13 +4,29 @@ describe Spree::SitesController do
 
   context "with host nonexistent" do
     before do
-      #@site1 = create(:site1)
-      #@site2 = create(:site2)
+      @site1 = create(:site1)
     end
 
     it "should response moved_permanently" do
       request.host = 'nonexistent' # it is localhost
       spree_get :one_click_trial
+      expect(response).to have_http_status(:moved_permanently)
+    end
+  end
+end
+
+describe Spree::PageLayoutsController do
+  stub_initialize_template!
+
+  context "with host nonexistent" do
+    let( :template_theme ){ create( :template_theme) }
+    let( :page_layout ){ create( :page_layout) }
+    before do
+      Spree::Site.current = create(:site1)
+    end
+    it "should response moved_permanently" do
+      request.host = 'nonexistent' # it is localhost
+      spree_get :edit,:template_theme_id => template_theme.id, :id => page_layout.id
       expect(response).to have_http_status(:moved_permanently)
     end
   end
