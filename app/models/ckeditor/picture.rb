320718f3bd6599cca80deafde25091f7a384c21b
david <areq22@aliyun.com>
2016-01-28 18:41:30 +0800

    fix ckeditor content image

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -3,13 +3,13 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :url  => "/shops/:rails_env/:site/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/shops/:rails_env/:site/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '800>', :thumb => '118x100#' },
-                    :default_style => :original
+                    :default_style => :content
 
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_content_type :data, :content_type => /\Aimage/
 
   def url_content
-    url(:content)
+    url(:original)
   end
 end
