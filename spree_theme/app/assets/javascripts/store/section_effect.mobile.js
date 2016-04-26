71eba5e21824a48d1c7f81260d97231d95614342
david <areq22@aliyun.com>
2016-04-26 14:19:36 +0800

    disable scroll event on overlay

--- spree_theme/app/assets/javascripts/store/section_effect.mobile.js
+++ spree_theme/app/assets/javascripts/store/section_effect.mobile.js
@@ -1,6 +1,13 @@
 
 $(function() {
-  $("#page").on( 'tap',".sidr_overlay",function(){
+  $("#page").on( 'tap', '.sidr_overlay', function(e) {
     $.sidr( 'close','sidr');
+    e.preventDefault();
   });
+  // prevent scrolling happen
+  $('#page').on('drag', '.sidr_overlay', function(e) {
+    // prevent default horizontal scrolling
+    e.preventDefault();
+  });
+
 });
