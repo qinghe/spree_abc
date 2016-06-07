97d32e57f2ec35ce31d71b37e1311f8abe14f384
david <areq22@aliyun.com>
2016-06-07 14:42:18 +0800

    improve feature ckeditor effect page

--- spree_theme/app/assets/javascripts/store/ckeditor_effect.js
+++ spree_theme/app/assets/javascripts/store/ckeditor_effect.js
@@ -9,11 +9,27 @@ $(document).ready(function() {
   //  </div>
   //</div>
 
-    $(".ck_effect_page_nav a").click(function(event){
+    $(".ck_effect_page_nav a.page").click(function(event){
       var $this =  $(this);
       var $page_content = $this.parent().siblings('.ck_effect_page_content');
-      var i = $this.prevAll().size();
-      $page_content.children().hide().eq(i).show();
+      var i = $this.prevAll('.page').size();
+      $page_content.children().removeClass('current').hide().eq(i).addClass('current').show();
+      event.preventDefault();
+    })
+    $(".ck_effect_page_nav a.next").click(function(event){
+      var $this =  $(this);
+      var $page_content = $this.parent().siblings('.ck_effect_page_content').children('.current');
+      if( $page_content.next().is('*')){
+        $page_content.removeClass('current').hide().next().addClass('current').show();
+      }
+      event.preventDefault();
+    })
+    $(".ck_effect_page_nav a.prev").click(function(event){
+      var $this =  $(this);
+      var $page_content = $this.parent().siblings('.ck_effect_page_content').children('.current');
+      if( $page_content.prev().is('*')){
+        $page_content.removeClass('current').hide().prev().addClass('current').show();
+      }
       event.preventDefault();
     })
 
