4350dad5c9a4b470acddb3182a949e46f3d27415
zhang ze <david@www.david.com>
2016-06-07 21:58:09 +0800

    add params slides for slider

--- spree_theme/db/seeds/200/241_slider_scrolling.rb
+++ spree_theme/db/seeds/200/241_slider_scrolling.rb
@@ -1,6 +1,6 @@
 
-post_summary = Spree::Section.create_section(section_piece_hash['container'], {:title=>"Slider scrolling"},
+slider = Spree::Section.create_section(section_piece_hash['container'], {:title=>"Slider scrolling"},
 {'block'=>{'disabled_ha_ids'=>'111','21unset'=>'0','21'=>'width:600px','15unset'=>'0','15'=>'height:80px'},
  'inner'=>{'15hidden'=>bool_true}})
-   
-post_summary.add_section_piece(section_piece_hash['slider-scrolling'])
+
+slider.add_section_piece(section_piece_hash['slider-scrolling'])
