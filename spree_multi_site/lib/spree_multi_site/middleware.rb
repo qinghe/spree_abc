46a2dc3f239937537827ee90b1b4cfe621d65f08
qinghe <david@getstore.cn>
2017-12-07 05:31:36 -0500

    improve multi site for development

--- spree_multi_site/lib/spree_multi_site/middleware.rb
+++ spree_multi_site/lib/spree_multi_site/middleware.rb
@@ -26,13 +26,14 @@ module SpreeMultiSite
       # apache rewrite test.david.com => localhost:8080/?n=test.david.com, request.host is 'test.david.com'
       # TODO should use public_suffix_service handle example.com.cn
 
-      if(( Rails.env !~ /prduction/ ) && ( store.blank? ) )
+      if(( Rails.env =~ /development|test/ ) && ( store.blank? ) )
         # for development or test, enable get site from cookie
         # string and symbol both OK.  cookie.domain should be exactly same as host, www.domain.com != domain.com
         # disable domain, some site have no domain, short_name always exists.
-        # we can not easily modify cookies except firefox, we'll add default_site for debug page on other browser.
-        # we could set default site for missing site as well.
-        short_name = request.cookies['_dalianshops_short_name']
+        # edit /etc/hosts file, add domains as below for development
+        # local test domains:
+        # first.david.com, design.david.com, demo.david.com
+        short_name = request.host.split('.').first
         if short_name.present?
           store = Spree::Store.unscoped.find_by_code( short_name )
         end
