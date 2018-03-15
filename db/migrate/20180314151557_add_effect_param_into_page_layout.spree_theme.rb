8adc426e1f197b903c7e22ae5d2bbfbd2b6f0df7
qinghe <david@getstore.cn>
2018-03-15 09:44:27 +0800

    add column effect_param

---
+++ db/migrate/20180314151557_add_effect_param_into_page_layout.spree_theme.rb
@@ -0,0 +1,13 @@
+# This migration comes from spree_theme (originally 20180314070030)
+class AddEffectParamIntoPageLayout < ActiveRecord::Migration[5.1]
+  def change
+    # 制作模板时，有很多特效，如 鼠标点击滑出，鼠标放上放大等
+    # 鼠标点击 和 鼠标放上特效，由 Html, js, css 配合实现
+    # effect_param 用来定义page_layout 的css class,
+    # 以便js找到相应元素，实现特效
+    # 示例：:hover_slide, :hover_show, :hover_expansion, :hover_overlay, :hover_popup, :popup_menu, :popup_menu_l, :sider,     :multi_level_menu
+    add_column :spree_page_layouts, :effect_param, :string, limit: 32
+
+  end
+
+end
