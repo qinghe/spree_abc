bbd6bd566a3f39e7df8517ca071e24b793705fad
qinghe <david@getstore.cn>
2018-06-06 14:06:58 +0800

    禁止 'X-Frame-Options' => 'SAMEORIGIN'

---
+++ config/initializers/default_header.rb
@@ -0,0 +1,5 @@
+SpreeAbc::Application.config.action_dispatch.default_headers = {
+  #'X-Frame-Options' => 'SAMEORIGIN',
+  'X-XSS-Protection' => '1; mode=block',
+  'X-Content-Type-Options' => 'nosniff'
+}
